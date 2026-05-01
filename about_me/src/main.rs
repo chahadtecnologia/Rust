fd2a8f5f1c07fb8319d6d04f3a627072a93e7e10
Marcio Gustavo Chahad <chahadtecnologia@gmail.com>
2026-04-30 21:29:43 -0300

    Adding first code files of Rust

---
+++ about_me/src/main.rs
@@ -0,0 +1,12 @@
+/* Code created to output information using Rust
+   To compile this project you can use one of the commands below:
+   - rustc <file.rs>
+   - cargo build
+   - cargo build --release
+   - cargo run
+*/
+fn main() {
+    println!("Hello, my name is Chahad.");
+    println!("I'm from Brazil.");
+    println!("And I'm working as a Cyber Security Architecure.");
+}
---
+++ hello_world/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // This is a comment in Rust
+    println!("Hello World!"); // This is a comment in Rust
+
+    /*
+    This is a comment in Rust
+    This is helpful for a large block of comments
+    */
+
+}
