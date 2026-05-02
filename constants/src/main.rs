a5051aaa7e87273f46bfb2c6fb0889ff1b118ae1
Marcio Gustavo Chahad <chahadtecnologia@gmail.com>
2026-05-01 21:35:00 -0300

    Adding example of constants on Rust

---
+++ constants/src/main.rs
@@ -0,0 +1,12 @@
+/*
+    A constant is a name assigned to a value. A constant's value cannot change.
+    A constant could be used in any scope.
+    A constant value is define when the program is running.
+*/
+
+const TAX_RATE: f64 = 7.25;
+
+fn main() {
+    let _income: i32 = 100000;
+    println!("My income is {} and the tax rate is {}", _income, TAX_RATE);
+}
