b731fb3aa8c32365bb65e3a2523795bfbf92cbb4
Marcio Gustavo Chahad <chahadtecnologia@gmail.com>
2026-05-13 21:40:06 -0300

    Adding examples of arrat type

---
+++ arraytype/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let _numbers: [i32; 6] = [4, 8, 15, 16, 23, 42];
+
+    let _fruit: [&str; 3] = ["Orange", "Pinapple", "Papaia"];
+    println!("Lenght: {}", _fruit.len());
+
+    let _currency_rates: [f64; 2] = [1.7, 7.4];
+    println!("Lenght: {}", _currency_rates.len());
+}
